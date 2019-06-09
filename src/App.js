b6fdefa1e34ff39a52ef4c6615a7adf71f909faa
Prateek Surana <prateeksurana3255@gmail.com>
2019-06-09 17:40:12 +0530

    :sparkles: added hooks to load the todo lists

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { name: "Do pushups", status: true },
+    { name: "Do situps", status: false }
+  ]);
   return (
     <div className="container">
       <p>
-        <label>Add Item</label><input id="new-task" type="text"/><button>Add</button>
+        <label>Add Item</label>
+        <input id="new-task" type="text" />
+        <button>Add</button>
       </p>
-      
+
       <h3>Todo</h3>
       <ul id="incomplete-tasks">
-        <li><input type="checkbox"/><label>Pay Bills</label><button className="delete">Delete</button></li>
-        
+        {todos
+          .filter(todo => !todo.status)
+          .map((todo,i) => (
+            <li key={i}>
+              <input type="checkbox" />
+              <label>{todo.name}</label>
+              <button className="delete">Delete</button>
+            </li>
+          ))}
       </ul>
-      
+
       <h3>Completed</h3>
       <ul id="completed-tasks">
-        <li><input type="checkbox" checked/><label>See the Doctor</label><button class="delete">Delete</button></li>
+      {todos
+          .filter(todo => todo.status)
+          .map((todo,i) => (
+            <li key={i}>
+              <input type="checkbox"  checked/>
+              <label>{todo.name}</label>
+              <button className="delete">Delete</button>
+            </li>
+          ))}
       </ul>
     </div>
   );
