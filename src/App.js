18beab2d454a7c5488b49ee69661e5de19be1bf6
Prateek Surana <prateeksurana3255@gmail.com>
2019-06-09 18:15:41 +0530

    :sparkles: added delete functionality

--- src/App.js
+++ src/App.js
@@ -7,38 +7,53 @@ function App() {
     { name: "Do situps", status: false, id: 2 }
   ]);
 
-  const [value, setValue]= useState("");
+  const [value, setValue] = useState("");
 
-  const onValueChange = ({target:{value}})=>{
-    setValue(value)
-  }
+  const onValueChange = ({ target: { value } }) => {
+    setValue(value);
+  };
 
-  const addTodo = ()=>{
-    if(value!==""){
-      setTodos([...todos, {name:value, status:false, id: todos.length + 1}])
-      setValue("")
+  const addTodo = () => {
+    if (value !== "") {
+      setTodos([
+        ...todos,
+        { name: value, status: false, id: todos.length + 1 }
+      ]);
+      setValue("");
     }
-  }
+  };
 
-  const handleKeyPress = ({key})=>{
-    if(key==='Enter'){
-      addTodo()
+  const handleKeyPress = ({ key }) => {
+    if (key === "Enter") {
+      addTodo();
     }
-  }
-  
-  const handleCheckboxChange = (id)=>{
-    setTodos(todos.map(todo=>{
-      if(todo.id===id)
-        return {...todo, status: !todo.status};
-      return todo;
-    }))
-  }
+  };
+
+  const handleCheckboxChange = id => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) return { ...todo, status: !todo.status };
+        return todo;
+      })
+    );
+  };
+
+  const deleteTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
 
   return (
     <div className="container">
       <p>
         <label>Add Item</label>
-        <input id="new-task" type="text" value={value} name="todoField" onKeyDown={handleKeyPress} onChange={onValueChange}/>
+        <input
+          id="new-task"
+          type="text"
+          value={value}
+          name="todoField"
+          onKeyDown={handleKeyPress}
+          onChange={onValueChange}
+        />
         <button onClick={addTodo}>Add</button>
       </p>
 
@@ -48,22 +63,34 @@ function App() {
           .filter(todo => !todo.status)
           .map(todo => (
             <li key={todo.id}>
-              <input type="checkbox" checked={todo.status} onChange={()=>handleCheckboxChange(todo.id)}/>
+              <input
+                type="checkbox"
+                checked={todo.status}
+                onChange={() => handleCheckboxChange(todo.id)}
+              />
               <label>{todo.name}</label>
-              <button className="delete">Delete</button>
+              <button className="delete" onClick={() => deleteTodo(todo.id)}>
+                Delete
+              </button>
             </li>
           ))}
       </ul>
 
       <h3>Completed</h3>
       <ul id="completed-tasks">
-      {todos
+        {todos
           .filter(todo => todo.status)
           .map(todo => (
             <li key={todo.id}>
-              <input type="checkbox" checked={todo.status} onChange={()=>handleCheckboxChange(todo.id)}/>
+              <input
+                type="checkbox"
+                checked={todo.status}
+                onChange={() => handleCheckboxChange(todo.id)}
+              />
               <label>{todo.name}</label>
-              <button className="delete">Delete</button>
+              <button className="delete" onClick={() => deleteTodo(todo.id)}>
+                Delete
+              </button>
             </li>
           ))}
       </ul>
